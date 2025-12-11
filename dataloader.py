6dfd1489f5f2b9350f366714d3c6d2f1e9d9d89d
ong shengda <ongshengda16@gmail.com>
2025-12-11 15:14:29 +0800

    dataloader added

---
+++ dataloader.py
@@ -0,0 +1,47 @@
+from dataset import ImageClassifcationDataset
+from torch.utils.data import DataLoader
+from transform import Compose, RandomHorizontalFlip, Resize, Normalise
+
+root_dir = ""
+train_csv = ""
+val_csv = ""
+
+
+train_transform = Compose([
+    Resize((244,244)),
+    RandomHorizontalFlip(p=0.5),
+    Normalise(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # ImageNet values
+])
+
+val_transform = Compose([
+    Resize((244,244)),
+    Normalise(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # ImageNet values
+])
+
+train_dataset = ImageClassifcationDataset(
+    csv_file=train_csv,
+    root_dir=root_dir,
+    transform=train_transform
+)
+
+val_dataset = ImageClassifcationDataset(
+    csv_file=train_csv,
+    root_dir=root_dir,
+    transform=val_transform
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=32,
+    shuffle = True,
+    num_workers = 4,
+    pin_memory = True # enable faster cpu to gpu transfer
+)
+
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=32,
+    shuffle = True,
+    num_workers = 4,
+    pin_memory = True # enable faster cpu to gpu transfer
+)
